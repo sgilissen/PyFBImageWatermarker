5859b1b2d31235364d03520b9ddae027f8de3a79
Gilissen Steve <steve@pulseconsulting.be>
2019-04-25 16:10:34 +0200

    Added FB post functionality. Still need to check image posting.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ This module takes a folder of image files, watermarks them and uploads them to a
 __author__ = "Steve Gilissen"
 __copyright__ = "Copyright 2018, Steve Gilissen"
 __credits__ = ["Steve Gilissen"]
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __maintainer__ = "Steve Gilissen"
 __status__ = "Development"
 
@@ -25,27 +25,40 @@ from os import path
 #                       GLOBAL VARIABLES
 # ---------------------------------------------------------
 configfile = "config.json"
-config = ""
-cfg = {
-    "page_id"      : "my_page_id",
-    "access_token" : "my_access_token"
-    }
+page_access_token = ''
+page_id = ''
 
-api = get_api(cfg)
+workdir = '_temp'
+workfile = ''
 
 # ---------------------------------------------------------
 #                       CORE FUNCTIONS
 # ---------------------------------------------------------
 parser = argparse.ArgumentParser(description='Watermark an image.')
-parser.add_argument('-i', '--input', action='store', dest='inputfile', help='Input file')
-parser.add_argument('-o', '--output', action='store', dest='outputfile', help='Output file')
+parser.add_argument('inputfile', help='Input file')
+parser.add_argument('outputfile', help='Output file')
 parser.add_argument('-w', '--watermark', action='store', dest='watermarkfile', help='Watermark file')
 args = parser.parse_args()
 
-def LogToConsole(message):
+
+def main():
+    log_to_console("Checking config...")
+    if args.watermarkfile is not None:
+        watermark_photo(args.inputfile, args.outputfile, args.watermarkfile, (0, 0))
+    else:
+        log_to_console("No watermark requested. Uploading to FB...")
+
+
+    if check_config():
+        post_to_fb("Another test", workfile)
+    else:
+        log_to_console("Please check your config.")
+
+def log_to_console(message):
     print("[" + str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')) + "] " + message)
 
-def WatermarkPhoto(input_path, output_path, watermark_path, position):
+
+def watermark_photo(input_path, output_path, watermark_path, position):
     base_image = Image.open(input_path)
     watermark = Image.open(watermark_path)
 
@@ -54,23 +67,59 @@ def WatermarkPhoto(input_path, output_path, watermark_path, position):
     transparent = Image.new('RGBA', (width, height), (0, 0, 0, 0))
     transparent.paste(base_image, (0, 0))
     transparent.paste(watermark, position, mask=watermark)
-    transparent.show()
-    transparent.save(output_image_path)
+    fill_color = (255, 255, 255)
+    if transparent.mode in ('RGBA', 'LA'):
+        background = Image.new(transparent.mode[:-1], transparent.size, fill_color)
+        background.paste(transparent, transparent.split()[-1])
+        transparent = background
+
+    transparent.save(output_path)
+    global workfile
+    workfile = output_path
+    print(workfile)
+
 
 def dir_path(path):
     if os.path.isdir(path):
         return path
     else:
-        #raise argparse.ArgumentTypeError(f"readable_dir:{path} is not a valid path")
-        LogToConsole("Folder: " + args.inputfile + " is not a valid path")
+        # raise argparse.ArgumentTypeError(f"readable_dir:{path} is not a valid path")
+        log_to_console("Folder: " + args.inputfile + " is not a valid path")
 
-def CheckConfig():
+
+def check_config():
     if path.exists(configfile):
         with open(configfile) as json_data_file:
             config = json.load(json_data_file)
+            try:
+                if config['page_id'] == "change me" or config['access_token'] == "change me":
+                    log_to_console("The config.json file is incorrect. Please modify the file to reflect your page.")
+                    return False
+                else:
+                    log_to_console("Config file found. Proceeding...")
+                    global page_access_token
+                    page_access_token = config['access_token']
+                    global page_id
+                    page_id = config['page_id']
+                    return True
+            except KeyError:
+                log_to_console("The config.json file is invalid.")
+                return False
     else:
-        defaultconfig = '{"page_id": "not_set", "access_token":"not_set"}'
+        defaultconfig = {"page_id": "change me", "access_token": "change me"}
+        log_to_console("Config file not found. A generic config.json has been created. "
+                       "Please modify to suit your needs.")
+        with open('config.json', 'w') as outfile:
+            json.dump(defaultconfig, outfile)
 
 
-LogToConsole("Input: " + args.inputfile)
-dir_path(args.inputfile)
+def post_to_fb(message, image):
+    graph = fb.GraphAPI(access_token=page_access_token, version='2.8')
+    # graph.put_object(page_id, connection_name='feed', message=message)
+    photo = open(image, "rb")
+    # graph.put_object(page_id, "photos", message="Watermarked doggo sez O HAI!", src=photo.read())
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
